94f2b53555d3ff03a781931986aed67066dce27d
Szczenathemonke <mpilkowski.learn@gmail.com>
2022-07-08 12:32:21 +0200

    zadanie1

---
+++ zad1.js
@@ -0,0 +1,36 @@
+const superGra = fetch("https://raider.io/api/v1/characters/profile?region=eu&realm=burning-legion&name=Nunijo")
+const statusCheck = document.getElementById("status")
+
+superGra.then((response) => {
+    if (response.ok){
+        statusCheck.textContent = "status Promise : Fulfilled"    
+    }else{
+        statusCheck.textContent = "coś się popsuło i nie było mnie słychać"
+    }
+    return response.json()
+}, (error)=> {
+    console.log(response.ok)
+    statusCheck.textContent = "coś się popsuło i nie było mnie słychać"
+}).then((body)=>{
+    console.log(body)
+    const superMonke = body
+
+    for (let key in superMonke) {
+       const objKey = document.createElement("p")
+       objKey.id = `${key}`
+       objKey.textContent = `${key}: ${superMonke[key]}`
+       document.body.appendChild(objKey)
+    }
+
+
+})
+
+
+    // if (response.ok){
+    //     statusCheck.textcontent = "gra i śpiewa, wszystko działa"        
+    // }else{
+    //     statusCheck.textContent = "coś się popsuło i nie było mnie słychać"
+    // } 
+
+
+
