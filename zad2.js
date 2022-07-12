6c9e3d4778bb8a0b4eab00740e54cb63a1251a8c
Szczenathemonke <mpilkowski.learn@gmail.com>
2022-07-12 13:17:17 +0200

    zad2

--- zad2.js
+++ zad2.js
@@ -3,53 +3,64 @@ const charRegion = document.getElementById("regionInput")
 const charRealm = document.getElementById("realmInput")
 const charName = document.getElementById("nameInput")
 
-const btnTest = document.getElementById("test")
-
+const btnTest = document.getElementById("btnTest")
+const charSheet = document.getElementById("charSheet")
 
 function getCharacter(charRegion, charRealm, charName){
-    const param1 = encodeURIComponent(charRegion)
-    const param2 = encodeURIComponent(charRealm)
-    const param3 = encodeURIComponent(charName)
+    const params = {
+        region: `${charRegion}`,
+        realm: `${charRealm}`,
+        name: `${charName}`
+    }
+    const searchParams = new URLSearchParams(params)
     
-    const character = fetch(`https://raider.io/api/v1/characters/profile?region=${param1}&realm=${param2}&name=${param3}`)
-    .then(res =>  res.json())  
-    // .then(error => console.error(error))  
-    // .then((body)=>{
-    //         console.log(body)
-    //         const superMonke = body
+    charSheet.innerHTML =""
+    let character = fetch(`https://raider.io/api/v1/characters/profile?${searchParams.toString()}`)
+    .then(res =>  res.json())      
+    .then((body)=>{
+            console.log(body)
+            const superMonke = body
         
-    //         for (const key in superMonke) {
-    //            const objKey = document.createElement("p")
-    //            objKey.id = `${key}`
-    //            objKey.textContent = `${key}: ${superMonke[key]}`
-    //            document.body.appendChild(objKey)
-    //         }
-    //     })
-    return console.log(character)
+            for (const key in superMonke) {
+               const objKey = document.createElement("p")
+               objKey.id = `${key}`
+               objKey.textContent = `${key}: ${superMonke[key]}`
+               charSheet.appendChild(objKey)
+            }
+        })
+    .then(error => console.error(error))
+    return character
 }
 
+
+
+
+// btnTest.addEventListener("click", ()=> {
+//     console.log(getCharacter(`${charRegion.textContent}`, `${charRealm.textContent}`, `${charName.textContent}`))
+// })
+
+
+// characterForm.addEventListener('change', changeCharacter())
+
+// function changeCharacter(){
+//         charRegion.value = charRegion.textContent
+//         charRealm.value = charRegion.textContent
+//         charName.value = charRegion.textContent
+// }
+
+
+// getCharacter(`${charRegion.value}`,`${charRealm.value}`,`${charName.value}`)
+// function changeCharacter(event){
+//     event.preventDefault()
+//     charRegion.value = charRegion.textContent
+//     charRealm.value = charRegion.textContent
+//     charName.value = charRegion.textContent
+// }
 // console.log(getCharacter("eu", "burning-legion", "nunijo"))
 // console.log(charRegion.value)
-btnTest.addEventListener("click", getCharacter(charRegion.value, charRealm.value, charName.value))
-// characterForm.addEventListener("submit", getCharacter)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// charName.addEventListener('input', getCharacter(`${charRegion.value}`,`${charRealm.value}`,`${charName.value}`))
+// getCharacter(`${charRegion.value}`,`${charRealm.value}`,`${charName.value}`)
 
 // const superGra = fetch("https://raider.io/api/v1/characters/profile?region=eu&realm=burning-legion&name=Nunijo")
 // const statusCheck = document.getElementById("status")
