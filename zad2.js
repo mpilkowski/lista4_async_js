b45254d3ea1d48ff2ff2b162610197f7f6fd0bdf
Szczenathemonke <mpilkowski.learn@gmail.com>
2022-08-10 09:51:49 +0200

    update
    function getCharacter dont affect DOM
    submit working

--- zad2.js
+++ zad2.js
@@ -8,17 +8,22 @@ const charSheet = document.getElementById("charSheet")
 
 function getCharacter(charRegion, charRealm, charName){
     const params = {
-        region: `${charRegion}`,
-        realm: `${charRealm}`,
-        name: `${charName}`
+        region: charRegion,
+        realm: charRealm,
+        name: charName
     }
     const searchParams = new URLSearchParams(params)
-    
+
+    return fetch(`https://raider.io/api/v1/characters/profile?${searchParams.toString()}`)
+}
+characterForm.addEventListener('submit', (event)=>{
+    event.preventDefault()  
     charSheet.innerHTML =""
-    let character = fetch(`https://raider.io/api/v1/characters/profile?${searchParams.toString()}`)
-    .then(res =>  res.json())      
-    .then((body)=>{
-            console.log(body)
+    const inputParams = [charRegion.value, charRealm.value, charName.value]
+    const character = getCharacter(inputParams[0], inputParams[1], inputParams[2])
+
+    character.then(res =>  res.json())   
+    .then((body)=>{            
             const superMonke = body
         
             for (const key in superMonke) {
@@ -28,63 +33,11 @@ function getCharacter(charRegion, charRealm, charName){
                charSheet.appendChild(objKey)
             }
         })
-    .then(error => console.error(error))
-    return character
-}
+    
+    for (const Param of inputParams){
+        if(Param.length === 0){
+            character.then(error => console.error("incorrect input, try again!"))  
+        }   
+    }    
+})
 
-
-
-
-// btnTest.addEventListener("click", ()=> {
-//     console.log(getCharacter(`${charRegion.textContent}`, `${charRealm.textContent}`, `${charName.textContent}`))
-// })
-
-
-// characterForm.addEventListener('change', changeCharacter())
-
-// function changeCharacter(){
-//         charRegion.value = charRegion.textContent
-//         charRealm.value = charRegion.textContent
-//         charName.value = charRegion.textContent
-// }
-
-
-// getCharacter(`${charRegion.value}`,`${charRealm.value}`,`${charName.value}`)
-// function changeCharacter(event){
-//     event.preventDefault()
-//     charRegion.value = charRegion.textContent
-//     charRealm.value = charRegion.textContent
-//     charName.value = charRegion.textContent
-// }
-// console.log(getCharacter("eu", "burning-legion", "nunijo"))
-// console.log(charRegion.value)
-
-// charName.addEventListener('input', getCharacter(`${charRegion.value}`,`${charRealm.value}`,`${charName.value}`))
-// getCharacter(`${charRegion.value}`,`${charRealm.value}`,`${charName.value}`)
-
-// const superGra = fetch("https://raider.io/api/v1/characters/profile?region=eu&realm=burning-legion&name=Nunijo")
-// const statusCheck = document.getElementById("status")
-
-// superGra.then((response) => {
-//     if (response.ok){
-//         statusCheck.textContent = "status Promise : Fulfilled"    
-//     }else{
-//         statusCheck.textContent = "coś się popsuło i nie było mnie słychać"
-//     }
-//     return response.json()
-// }, (error)=> {
-//     console.log(response.ok)
-//     statusCheck.textContent = "coś się popsuło i nie było mnie słychać"
-// }).then((body)=>{
-//     console.log(body)
-//     const superMonke = body
-
-//     for (let key in superMonke) {
-//        const objKey = document.createElement("p")
-//        objKey.id = `${key}`
-//        objKey.textContent = `${key}: ${superMonke[key]}`
-//        document.body.appendChild(objKey)
-//     }
-
-
-// })
