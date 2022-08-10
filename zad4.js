eacaa8f00d4cfd133b142d7699254cfe9da9208f
Szczenathemonke <mpilkowski.learn@gmail.com>
2022-08-10 10:28:45 +0200

    rest parameters added

--- zad4.js
+++ zad4.js
@@ -1,12 +1,14 @@
 // func => func => promise
 function runAfter(time, func){
-    return (x,y)=>  {
-      let result = func(x,y)
-      return new Promise((res)=>{
-        setTimeout(res(result), time)
-      })   
-    } 
-  }
+  return (...params)=>  {
+    
+    return new Promise((resolve)=>{      
+      setTimeout(()=> {        
+        resolve(func(...params))
+      }, time)
+    })   
+  } 
+}
   
   const delayedAdd = runAfter(2000, (x, y) => x + y);
   delayedAdd(2, 4).then((r) => console.log("2 + 4 =", r));
